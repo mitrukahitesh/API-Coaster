bc4bf0a4ba37a2d8f26295774238cfd6073f263b
mitrukahitesh <mitrukahitesh@gmail.com>
2021-07-11 17:39:21 +0530

    added logout feature

--- script/signin.js
+++ script/signin.js
@@ -1,4 +1,8 @@
 const button = document.querySelector("[sign-in-button]");
+const navBar = document.querySelector("[nav-bar]");
+const dp = document.querySelector("[user-dp]");
+const username = document.querySelector("[user-name]");
+const signOutButton = document.querySelector("[sign-out-button]");
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -8,8 +12,32 @@ button.addEventListener("click", () => {
     .signInWithPopup(provider)
     .then((res) => {
       console.log(res);
+      navBar.style.display = "block";
+      username.textContent = res.user.displayName;
+      dp.setAttribute("src", res.user.photoURL);
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+function checkAuthState() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      navBar.style.display = "block";
+      button.style.display = "none";
+      username.textContent = user.displayName;
+      dp.setAttribute("src", user.photoURL);
+    } else {
+      navBar.style.display = "none";
+      button.style.display = "block";
+    }
+  });
+}
+
+signOutButton.addEventListener("click", () => {
+  firebase.auth().signOut();
+  checkAuthState();
+});
+
+checkAuthState();

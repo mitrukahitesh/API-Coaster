a3642df31e4c3bf8cf86b95de2d695f1207b6c97
mitrukahitesh <mitrukahitesh@gmail.com>
2021-08-22 10:38:51 +0530

    added secrets

---
+++ secrets.js
@@ -0,0 +1,11 @@
+const secrets = {
+  apiKey: "AIzaSyChiAWVuAh8VJByGJ_WxuICfaHjb9VrYq0",
+  authDomain: "api-coaster.firebaseapp.com",
+  projectId: "api-coaster",
+  storageBucket: "api-coaster.appspot.com",
+  messagingSenderId: "858146009539",
+  appId: "1:858146009539:web:ef0fe0ad583a817d9b0d76",
+  measurementId: "G-8778N88LSD",
+};
+
+export default secrets;
